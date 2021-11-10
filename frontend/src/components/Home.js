49822501ae781c35f6cfb20cb5c64013c35deb9e
adityatheprogrammer <adithya506@gmail.com>
2021-11-10 08:17:14 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,51 +1,48 @@
 import React, { Fragment, useEffect } from "react";
 import MetaData from "./layout/MetaData";
-import Product from "./product/Product"
-import { useDispatch,useSelector } from "react-redux";
+import Product from "./product/Product";
+import { useDispatch, useSelector } from "react-redux";
 
 import Loader from "./layout/Loader";
 import { useAlert } from "react-alert";
 import { getProducts } from "../actions/productActions";
-const Home=()=>{
-  const alert=useAlert();
-    const dispatch = useDispatch()
+const Home = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
 
-    const {loading,products,error,productsCount}=useSelector(state=>state.products)
-    
-    useEffect(() => {
-      
+  const { loading, products, error, productsCount } = useSelector(
+    (state) => state.products
+  );
 
+  useEffect(() => {
+    if (error) {
+      return alert.error(error);
+    }
+    console.log(error);
 
-      if (error) {
-          return alert.error(error)
-      }
-      console.log(error)
-      
-      dispatch(getProducts());
-    
-    },[dispatch, alert,error])
+    dispatch(getProducts());
+  }, [dispatch, alert, error]);
 
-    return(
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader>loading...</Loader>
+      ) : (
         <Fragment>
-          {loading ? <Loader>loading...</Loader>:(
-            <Fragment>
-         <MetaData title={'Buy Best products online'}/>
-            <h1 id="products_heading">Latest products</h1>
-            <section id="products" className="container mt-5">
-      <div className="row">
-        {products && products.map(product=>(
-          <Product key={product._id}product={product}/>
-        ))}
-        
-        
-      </div>
-    </section>
+          <MetaData title={"Buy Best products online"} />
+          <h1 id="products_heading">Latest products</h1>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {products &&
+                products.map((product) => (
+                  <Product key={product._id} product={product} />
+                ))}
+            </div>
+          </section>
+        </Fragment>
+      )}
     </Fragment>
-        
-            
-    )}
-    </Fragment>
-    )
-}
+  );
+};
 
-export default Home
+export default Home;
